a36dba28d3c1953e2a20a8d513b2c5b769a7af9a
maldrpn0309 <bagjinhyeong640@gmail.com>
2023-06-12 20:43:05 +0900

    연산자 기초 부분까지 중간저장

--- 08FuncEx/Program.cs
+++ 08FuncEx/Program.cs
@@ -1,16 +1,34 @@
-﻿
-class Player
+﻿class Player
 {
     // 접근제한 지정자를 입력하지 않으면
-    // 기본값으로로 private
+    // 기본값으로 private
     // private이 일반적이기 때문이다
     // 속성들은
     // 외부에서 접근하지 못하는게 일반적으로 좋다
     // 공격력은 공격력 그 자체만으로 의미가 있지만
     // 그 의미가 명확해지지 않는다
     // 공격력이 증가한다
+    private int LV = 1;
     private int ATT = 10;
     private int HP = 100;
+
+    // 플레이어의 레벨이 얼마인지 알고 샆다
+    // 인자 값이 아니고
+    // 리턴값을 사용
+    // 리턴값이란 객체가 자신의 상태를 외부에 알려주는
+    // 용도로 사용한다
+    // 외부에 알려줘야 하기 때문에 알려주는 순간
+    // 함수가 끝나게 된다
+    // 이 return 이라는 문장을 사용하면
+    // 얼마나 많은 코드가 return 아래에 있든 함수가 종료하게 된다
+    public int GetLv()
+    {
+        // 리턴을 하는 순간 함수가 끝나고
+        // 아래에 친 코드들은 모두 의미가 없다
+        LV = 10000;
+
+        return LV;
+    }
 
     // 상태라는건 맴버변수
     // 어떤 상태가 변화하는 순간
@@ -30,13 +48,14 @@ class Player
     }
 
     // 함수는 보통 선언과
+    // 내용으로 나뉘게
+    // 함수는 보통 선언과
     // 내용으로 나뉘게 되는데
     // 함수란 보통 클래스 외부와의 소통을 위해서 만든다   
-
     // void[리턴값] Func[이름 혹은 식별자] ()[인자값]
     public void Func(int _Dmg)
     {
-
+        // 코드 내용을 구현해야 함
     }
 
     // (int _Dmg)를 넣는다는 것은 외부에서
@@ -45,6 +64,14 @@ class Player
     public void Damage1(int _Dmg)
     {
         HP = HP - _Dmg;
+    }
+
+    // 리턴값은 자신이 리턴해주려는 자료형과
+    // 동일한 자료형 이어야 한다
+    public int DamageToHPReturn(int _Dmg)
+    {
+        HP = HP - _Dmg;
+        return HP;
     }
 
     // 두개가 될수도 있다
@@ -61,28 +88,28 @@ class Player
 
 namespace _08FuncEx
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-           Player NewPlayer = new Player();
+            Player NewPlayer = new Player();
             // 그래서 필요한 것이 함수이다
-
             // 뭔가 증가하는 코드를 추가해줘야 하는데
             // NewPlayer.HP = NewPlayer.HP + 1;
             // NewPlayer.HP = NewPlayer.HP + 1;
             // NewPlayer.ATT = NewPlayer.ATT + 1;
             NewPlayer.LVUP();
-
             NewPlayer.SetHP(0);
 
             // 이런식으로 외부의 값을 받아서
             // 객체가 내부의 상태를 변화시키기 위해서
             // 함수를 선언하는 경우가 많다
-            NewPlayer.Damage1(10);
-            NewPlayer.Damage1(20);
-            NewPlayer.Damage2 (10,20);
+            // NewPlayer.Damage1(10);
+            // NewPlayer.Damage1(20);
+            // NewPlayer.Damage2(10,20);
 
+            Console.WriteLine(NewPlayer.GetLv());
+            Console.WriteLine(NewPlayer.DamageToHPReturn(50));
         }
     }
 }
---
+++ 09Operator/Program.cs
@@ -0,0 +1,39 @@
+﻿
+class Player
+{
+    int HP = 100;
+
+    void Damage(int _HP)
+    {
+        HP = HP - 10;
+    }
+}
+
+
+
+namespace _09Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Result = 0;
+            int Left = 7;
+            int Right = 3;
+
+            // = 대입연산자
+            // 함수에서 리턴된 값이나
+            // 혹은 연산이된 값을
+            // 리터럴 값일때도 있다
+            // 그 값을 받을 수 있는 변수
+            // 넣어줘야 한다
+
+            // 산술연산자
+            Result = Left + Right; // 덧셈
+            Result = Left - Right; // 뺄셈
+            Result = Left * Right; // 곱셈
+            Result = Left / Right; // 나누기
+            Result = Left % Right; // 나머지
+        }
+    }
+}
